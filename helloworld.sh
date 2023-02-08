fa495b294bee1a290a90585775fb23a35be0aa81
Nicholas Loiacono <105306699+NicholasLoiacono@users.noreply.github.com>
2023-02-07 23:29:24 -0800

    Added script.

--- helloworld.sh
+++ helloworld.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Script: "I am working perfectly"
+# Script: Ops 201d6 Class 02 Ops Challenge Solution
 # Author: Nicholas Loiacono
 # Date: 02/07/2023
 # Purpose: Print a string to the terminal
 
 
 # Main
-
+"I am working perfectly"
 # End
