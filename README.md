262822532eda478035f9de9c61284e65ddf5bd1e
NicholasLoiacono <ntloiacono4@gmail.com>
2023-02-08 03:27:30 +0000

    Add file and modify README.md

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ops-201d6-code-challenges
 
 This is for my code challenges in class.
+
+Modified
---
+++ code-challenge-class 02.sh
@@ -0,0 +1,14 @@
+Part 1: Shelling In
+First, if you haven’t already, setup VS Code as per today’s lab assignment to shell into your dedicated scripting VM. See lab assignment for details.
+
+Part 2: Creating and Running the Script
+In your SSH session, use the touch command to create helloworld.sh in your git-initialized directory.
+Open helloworld.sh with VS Code
+Using the template below as your guide, create the script that will print “I am working perfectly” to the screen.
+Test your script by running it from the command line in SSH, like this: bash helloworld.sh
+Execution Instructions
+For this (and all) automation challenge scripts in this class, start with the template below, so that you and others know why this script was created, what purpose it serves, and how to use it properly.
+
+Add details to each section in the header
+
+Write your code under the # Main section
