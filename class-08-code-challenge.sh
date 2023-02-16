162cc89f4ae461b5eaa59226bfb0205e1235418d
NicholasLoiacono <ntloiacono4@gmail.com>
2023-02-16 17:41:14 +0000

    Updated code

--- class-08-code-challenge.sh
+++ class-08-code-challenge.sh
@@ -1,1 +1,10 @@
-ROBOCOPY
+@echo off
+
+set source=%userprofile%\Desktop\work
+set destination=D:\backup
+
+if not exist "%destination%" (
+  mkdir "%destination%"
+)
+
+robocopy "%source%" "%destination%" /E /LOG+:D:\backup\backup.log
