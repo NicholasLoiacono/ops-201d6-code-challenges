ba10a2d5229173c1dcd8771de0fecf0f1f983937
NicholasLoiacono <ntloiacono4@gmail.com>
2023-02-22 03:55:18 +0000

    Powershell regarding IP version 4

---
+++ class-12-code-challenge.sh
@@ -0,0 +1,23 @@
+Nicholas Loiacono 2/21/2023
+
+# Define a variable to hold the name of the output file
+$outputFile = "network_report.txt"
+
+# Define a function to get the IPv4 address from a given input string
+function Get-IPv4Address ($inputString) {
+    $regex = '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'  # Regular expression to match IPv4 addresses
+    $ipAddress = Select-String -InputObject $inputString -Pattern $regex | Select-Object -ExpandProperty Matches | Select-Object -ExpandProperty Value
+    return $ipAddress
+}
+
+# Run the ipconfig /all command and save the output to the output file
+ipconfig /all | Out-File $outputFile
+
+# Search the output file for IPv4 addresses using the Get-IPv4Address function
+$ipv4Address = Get-IPv4Address (Get-Content $outputFile)
+
+# Print the IPv4 address to the console
+Write-Host "IPv4 Address: $ipv4Address"
+
+# Remove the output file
+Remove-Item $outputFile
